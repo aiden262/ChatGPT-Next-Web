fda202486343ec0dbea7810fd5d89364cb3e211a
aiden262 <jiawei26@gmail.com>
2024-06-25 17:55:36 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,9 +39,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <link rel="manifest" href="/site.webmanifest"></link>
         <script src="/serviceWorkerRegister.js" defer></script>
-        <script>
-!function(p){"use strict";!function(t){var s=window,e=document,i=p,c="".concat("https:"===e.location.protocol?"https://":"http://","sdk.51.la/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:"KU4LkTcD1U1m792W",ck:"KU4LkTcD1U1m792W"});
-</script>
+        <script charset="UTF-8" id="LA_COLLECT" src="//sdk.51.la/js-sdk-pro.min.js?id=KU4LkTcD1U1m792W&ck=KU4LkTcD1U1m792W"></script>
       </head>
       <body>
         {children}
